fe2a6245985ae2a5ff873cc569ff4c352fc5bfe6
Anzamul Haque Akash <81710865+Anzamul-Haque-Akash@users.noreply.github.com>
2024-09-20 01:46:30 +0600

    CharacterAiming.cs Update

--- Assets/Scripts/Character Scripts/CharacterAiming.cs
+++ Assets/Scripts/Character Scripts/CharacterAiming.cs
@@ -4,6 +4,20 @@ namespace Character_Scripts
 {
     public class CharacterAiming : MonoBehaviour
     {
+        [SerializeField] private float m_TurnSpeed;
         
+        private Camera _mainCamera;
+
+        private void Start()
+        {
+            _mainCamera = Camera.main;
+        }
+
+        private void FixedUpdate()
+        {
+            float yawCamera = _mainCamera.transform.eulerAngles.y;
+            
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0f, yawCamera, 0f), m_TurnSpeed * Time.fixedDeltaTime);
+        }
     }
 }
