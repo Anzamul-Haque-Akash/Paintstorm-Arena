36469674374f192c7130b92ce1a58aec3623dc51
Anzamul Haque Akash <81710865+Anzamul-Haque-Akash@users.noreply.github.com>
2024-09-20 01:29:23 +0600

    CharacterLocomotion.cs Update

---
+++ Assets/Scripts/Character Scripts/CharacterLocomotion.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+namespace Character_Scripts
+{
+    public class CharacterLocomotion : MonoBehaviour
+    {
+        [SerializeField] private Animator m_Animator;
+
+        private Vector2 _input;
+        private static readonly int InputX = Animator.StringToHash("InputX");
+        private static readonly int InputY = Animator.StringToHash("InputY");
+
+        private void Update()
+        {
+            _input.x = Input.GetAxis("Horizontal");
+            _input.y = Input.GetAxis("Vertical");
+            
+            m_Animator.SetFloat(InputX, _input.x);
+            m_Animator.SetFloat(InputY, _input.y);
+        }
+    }
+}
