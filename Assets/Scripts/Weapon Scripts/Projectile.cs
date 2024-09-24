3d2ec92c9529e4919618ae935b0b729a40016080
Anzamul Haque Akash <81710865+Anzamul-Haque-Akash@users.noreply.github.com>
2024-09-24 22:51:57 +0600

    Shooting a weapon using Projectile Raycasts and projectile Update

---
+++ Assets/Scripts/Weapon Scripts/Projectile.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+namespace Weapon_Scripts
+{
+    public class Projectile : MonoBehaviour
+    {
+    }
+}
--- Assets/Scripts/Weapon Scripts/RaycastWeapon.cs
+++ Assets/Scripts/Weapon Scripts/RaycastWeapon.cs
@@ -5,10 +5,14 @@ namespace Weapon_Scripts
     public class RaycastWeapon : MonoBehaviour
     {
         public Transform m_RayCastOrigin;
+        public Transform m_RatcastDestination;
+        public GameObject m_Projectile;
+        public float m_ProjectileForce;
         public bool m_IsFiring;
 
         private Ray _ray;
         private RaycastHit _hitInfo;
+        private Vector3 _direction;
         
         private void Start()
         {
@@ -19,11 +23,17 @@ namespace Weapon_Scripts
         {
             m_IsFiring = true;
 
+            _direction = m_RatcastDestination.position - m_RayCastOrigin.position;
+
             _ray.origin = m_RayCastOrigin.position;
-            _ray.direction = m_RayCastOrigin.forward;
+            _ray.direction = _direction;
+            
+            GameObject projectile = Instantiate(m_Projectile, _ray.origin, Quaternion.identity);
+            Rigidbody projectileRb = projectile.GetComponent<Rigidbody>();
             
             if(Physics.Raycast(_ray, out _hitInfo))
             {
+                projectileRb.AddForce(_direction * m_ProjectileForce, ForceMode.Impulse);
                 Debug.DrawLine(_ray.origin, _hitInfo.point, Color.red, 1f);
             }
         }
