1ed27d9ce39b1ebbe38fba67e85b4bd59576848d
Anzamul Haque Akash <81710865+Anzamul-Haque-Akash@users.noreply.github.com>
2024-09-25 18:03:36 +0600

    Player recoil Update

--- Assets/Scripts/Weapon Scripts/WeaponRecoil.cs
+++ Assets/Scripts/Weapon Scripts/WeaponRecoil.cs
@@ -6,9 +6,12 @@ namespace Weapon_Scripts
     {
         public Cinemachine.CinemachineFreeLook m_PlayerCamera;
         public Cinemachine.CinemachineImpulseSource m_CameraShake;
-        public float m_VerticalRecoil;
+        public Vector2[] m_RecoilPattern;
         public float m_Duration;
-
+        
+        private float _verticalRecoil;
+        private float _horizontalRecoil;
+        private int _index;
         private float _time;
         private Camera _camera;
 
@@ -22,13 +25,20 @@ namespace Weapon_Scripts
             _time = m_Duration;
             
             m_CameraShake.GenerateImpulse(_camera.transform.forward);
-        }
 
+            _index = Random.Range(0, m_RecoilPattern.Length);
+            _horizontalRecoil = m_RecoilPattern[_index].x;
+            
+            _index = Random.Range(0, m_RecoilPattern.Length);
+            _verticalRecoil = m_RecoilPattern[_index].y;
+        }
+        
         private void Update()
         {
             if (_time > 0)
             {
-                m_PlayerCamera.m_YAxis.Value -= (m_VerticalRecoil * Time.deltaTime) / m_Duration;
+                m_PlayerCamera.m_YAxis.Value -= (_verticalRecoil * Time.deltaTime) / m_Duration;
+                m_PlayerCamera.m_XAxis.Value -= (_horizontalRecoil * Time.deltaTime) / m_Duration;
                 _time -= Time.deltaTime;
             }
         }
