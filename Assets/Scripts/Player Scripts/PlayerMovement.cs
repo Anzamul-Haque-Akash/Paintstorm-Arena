6434dd5472889557700166d6af484c23c5150fb7
Anzamul Haque Akash <81710865+Anzamul-Haque-Akash@users.noreply.github.com>
2024-09-29 01:31:18 +0600

    Player Jump Update

--- Assets/Scripts/Player Scripts/PlayerMovement.cs
+++ Assets/Scripts/Player Scripts/PlayerMovement.cs
@@ -8,6 +8,9 @@ namespace Player_Scripts
         [SerializeField] private CharacterController m_CharacterController;
         [SerializeField] private float m_JumpHeight;
         [SerializeField] private float m_Gravity;
+        [SerializeField] private float m_SpetDown;
+        [SerializeField] private float m_AirControl;
+        [SerializeField] private float m_JumpDamp;
         
         private Vector2 _input;
         private Vector3 _rootMotion;
@@ -36,23 +39,37 @@ namespace Player_Scripts
             if (_isJumping)
             {
                 _velocity.y -= m_Gravity * Time.fixedDeltaTime;
-                m_CharacterController.Move(_velocity * Time.fixedDeltaTime);
+                Vector3 displacement = _velocity * Time.fixedDeltaTime;
+                displacement += CalculateAirControle();
+                m_CharacterController.Move(displacement);
                 _isJumping = !m_CharacterController.isGrounded;
                 _rootMotion = Vector3.zero;
             }
             else
             {
-                m_CharacterController.Move(_rootMotion);
+                m_CharacterController.Move(_rootMotion + Vector3.down * m_SpetDown);
                 _rootMotion = Vector3.zero;
+
+                if (!m_CharacterController.isGrounded) 
+                {
+                    _isJumping = true;
+                    _velocity = m_Animator.velocity * m_JumpDamp;
+                    _velocity.y = 0f;
+                }
             }
         }
+
+        private Vector3 CalculateAirControle()
+        {
+            return ((transform.forward * _input.y) + (transform.right * _input.x)) * (m_AirControl / 100);
+        } 
 
         private void Jump()
         {
             if (!_isJumping)
             {
                 _isJumping = true;
-                _velocity = m_Animator.velocity;
+                _velocity = m_Animator.velocity * m_JumpDamp;
                 _velocity.y = Mathf.Sqrt(2f * m_Gravity * m_JumpHeight);
             }
         }
