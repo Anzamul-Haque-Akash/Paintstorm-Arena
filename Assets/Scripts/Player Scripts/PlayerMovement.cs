10d489ba4311fde7200ab020883ec029717f4eed
Anzamul Haque Akash <81710865+Anzamul-Haque-Akash@users.noreply.github.com>
2024-09-29 01:08:51 +0600

    Player Jump Update

--- Assets/Scripts/Player Scripts/PlayerMovement.cs
+++ Assets/Scripts/Player Scripts/PlayerMovement.cs
@@ -5,8 +5,15 @@ namespace Player_Scripts
     public class PlayerMovement : MonoBehaviour
     {
         [SerializeField] private Animator m_Animator;
-
+        [SerializeField] private CharacterController m_CharacterController;
+        [SerializeField] private float m_JumpHeight;
+        [SerializeField] private float m_Gravity;
+        
         private Vector2 _input;
+        private Vector3 _rootMotion;
+        private Vector3 _velocity;
+        private bool _isJumping;
+        
         private static readonly int InputX = Animator.StringToHash("InputX");
         private static readonly int InputY = Animator.StringToHash("InputY");
 
@@ -17,6 +24,37 @@ namespace Player_Scripts
             
             m_Animator.SetFloat(InputX, _input.x, 0.1f, Time.deltaTime);
             m_Animator.SetFloat(InputY, _input.y, 0.1f, Time.deltaTime);
+            
+            if(Input.GetKeyDown(KeyCode.Space)) Jump();
+        }
+        private void OnAnimatorMove()
+        {
+            _rootMotion += m_Animator.deltaPosition;
+        }
+        private void FixedUpdate()
+        {
+            if (_isJumping)
+            {
+                _velocity.y -= m_Gravity * Time.fixedDeltaTime;
+                m_CharacterController.Move(_velocity * Time.fixedDeltaTime);
+                _isJumping = !m_CharacterController.isGrounded;
+                _rootMotion = Vector3.zero;
+            }
+            else
+            {
+                m_CharacterController.Move(_rootMotion);
+                _rootMotion = Vector3.zero;
+            }
+        }
+
+        private void Jump()
+        {
+            if (!_isJumping)
+            {
+                _isJumping = true;
+                _velocity = m_Animator.velocity;
+                _velocity.y = Mathf.Sqrt(2f * m_Gravity * m_JumpHeight);
+            }
         }
     }
 }
