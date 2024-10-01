4d3634f43e55c94f8fa5122b79b07ebc727fe4ee
Anzamul Haque Akash <81710865+Anzamul-Haque-Akash@users.noreply.github.com>
2024-10-02 01:14:38 +0600

    Player State Machine Update

--- Assets/Scripts/Controllers/JumpController.cs
+++ Assets/Scripts/Controllers/JumpController.cs
@@ -1,9 +1,83 @@
+using Constants;
+using Player_Scripts;
 using UnityEngine;
 
 namespace Controllers
 {
     public class JumpController : MonoBehaviour
     {
+        [SerializeField] private Animator m_Animator;
+        [SerializeField] private CharacterController m_CharacterController;
+        [SerializeField] private MovementController m_MovementController;
         
+        private Vector2 _input;
+        private float _jumpDamp;
+        private bool _isJumping;
+        private Vector3 _velocity;
+        
+        private void Start()
+        {
+            _jumpDamp = Player.Instance.PlayerData.m_JumpDamp;
+        }
+        
+        private void Update()
+        {
+            _input.x = Input.GetAxis("Horizontal");
+            _input.y = Input.GetAxis("Vertical");
+
+            if (Input.GetKeyDown(KeyCode.Space)) Jump();
+            
+            m_MovementController.enabled = !_isJumping;
+        }
+        
+        private void FixedUpdate()
+        {
+            if (_isJumping) UpdateInAir();
+
+            if (m_CharacterController.isGrounded)
+            {
+                if (!m_CharacterController.isGrounded) SetInAir(0f);
+            }
+        }
+        
+        private void Jump()
+        {
+            if (!_isJumping)
+            {
+                float jumpVelocity = Mathf.Sqrt(2f * Player.Instance.PlayerData.m_Gravity * Player.Instance.PlayerData.m_JumpHeight);
+                SetInAir(jumpVelocity);
+                
+                m_MovementController.enabled = !_isJumping;
+            }
+        }
+        
+        private void UpdateInAir()
+        {
+            _velocity.y -= Player.Instance.PlayerData.m_Gravity * Time.fixedDeltaTime;
+            Vector3 displacement = _velocity * Time.fixedDeltaTime;
+            displacement += CalculateAirController();
+            m_CharacterController.Move(displacement);
+            _isJumping = !m_CharacterController.isGrounded;
+            m_Animator.SetBool(AnimatorHashes.IsJumping, _isJumping);
+        }
+        
+        private void SetInAir(float jumpVelocity)
+        {
+            _isJumping = true;
+            _velocity = m_Animator.velocity * (_jumpDamp * Player.Instance.PlayerData.m_GroundSpeed);
+            _velocity.y = jumpVelocity;
+            m_Animator.SetBool(AnimatorHashes.IsJumping, true);
+        }
+        
+        private Vector3 CalculateAirController()
+        {
+            return ((transform.forward * _input.y) + (transform.right * _input.x)) * (Player.Instance.PlayerData.m_AirControl / 100);
+        }
+
+        private void HandelGravity()
+        {
+            Vector3 stepDownAmunt = Vector3.down * Player.Instance.PlayerData.m_StepDown;
+            m_CharacterController.Move(stepDownAmunt);
+        }
     }
 }
--- Assets/Scripts/Controllers/MovementController.cs
+++ Assets/Scripts/Controllers/MovementController.cs
@@ -12,16 +12,13 @@ namespace Controllers
         private Vector2 _input;
         private Vector3 _rootMotion;
         private float _groundSpeed;
-        private float _jumpDamp;
         private Vector3 _velocity;
-        private bool _isJumping;
         private bool _isCrouching;
         private float _animatorWeight;
 
         private void Start()
         {
             _groundSpeed = Player.Instance.PlayerData.m_GroundSpeed;
-            _jumpDamp = Player.Instance.PlayerData.m_JumpDamp;
         }
 
         private void Update()
@@ -31,18 +28,14 @@ namespace Controllers
 
             m_Animator.SetFloat(AnimatorHashes.InputX, _input.x, 0.1f, Time.deltaTime);
             m_Animator.SetFloat(AnimatorHashes.InputY, _input.y, 0.1f, Time.deltaTime);
-
-            if (Input.GetKeyDown(KeyCode.Space)) Jump();
-
+            
             if (Input.GetKey(KeyCode.LeftShift))
             {
                 _groundSpeed = Player.Instance.PlayerData.m_GroundMaxSpeed;
-                _jumpDamp = Player.Instance.PlayerData.m_MaxJumpDamp;
             }
             else
             {
                 _groundSpeed = Player.Instance.PlayerData.m_GroundSpeed;
-                _jumpDamp = Player.Instance.PlayerData.m_JumpDamp;
             }
 
             if (Input.GetKeyDown(KeyCode.C))
@@ -60,8 +53,7 @@ namespace Controllers
 
         private void FixedUpdate()
         {
-            if (_isJumping) UpdateInAir();
-            else UpdateOnGround();
+            UpdateOnGround();
         }
 
         private void UpdateOnGround()
@@ -71,50 +63,13 @@ namespace Controllers
 
             m_CharacterController.Move(stepForwardAmount + stepDownAmunt);
             _rootMotion = Vector3.zero;
-
-            if (!m_CharacterController.isGrounded) SetInAir(0f);
         }
-
-        private void UpdateInAir()
-        {
-            _velocity.y -= Player.Instance.PlayerData.m_Gravity * Time.fixedDeltaTime;
-            Vector3 displacement = _velocity * Time.fixedDeltaTime;
-            displacement += CalculateAirController();
-            m_CharacterController.Move(displacement);
-            _isJumping = !m_CharacterController.isGrounded;
-            _rootMotion = Vector3.zero;
-            m_Animator.SetBool(AnimatorHashes.IsJumping, _isJumping);
-        }
-
-        private Vector3 CalculateAirController()
-        {
-            return ((transform.forward * _input.y) + (transform.right * _input.x)) *
-                   (Player.Instance.PlayerData.m_AirControl / 100);
-        }
-
-        private void Jump()
-        {
-            if (!_isJumping)
-            {
-                float jumpVelocity = Mathf.Sqrt(2f * Player.Instance.PlayerData.m_Gravity *
-                                                Player.Instance.PlayerData.m_JumpHeight);
-                SetInAir(jumpVelocity);
-            }
-        }
-
-        private void SetInAir(float jumpVelocity)
-        {
-            _isJumping = true;
-            _velocity = m_Animator.velocity * (_jumpDamp * _groundSpeed);
-            _velocity.y = jumpVelocity;
-            m_Animator.SetBool(AnimatorHashes.IsJumping, true);
-        }
-
+        
         private void SetAnimationLayerWeight()
         {
-            _animatorWeight = Mathf.Lerp(_animatorWeight, _isCrouching ? 1 : 0,
-                Time.deltaTime * Player.Instance.PlayerData.m_CrouchSpeed);
+            _animatorWeight = Mathf.Lerp(_animatorWeight, _isCrouching ? 1 : 0, Time.deltaTime * Player.Instance.PlayerData.m_CrouchSpeed);
             m_Animator.SetLayerWeight(1, _animatorWeight);
         }
+        
     }
 }
