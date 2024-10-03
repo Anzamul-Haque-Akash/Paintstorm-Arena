05eb515c2c5a4bd125dee6d21c77416f01f0c9d9
Anzamul Haque Akash <81710865+Anzamul-Haque-Akash@users.noreply.github.com>
2024-10-03 23:48:35 +0600

    Player FSM Update

--- Assets/Scripts/State Machine/PlayerStateManager.cs
+++ Assets/Scripts/State Machine/PlayerStateManager.cs
@@ -1,17 +1,8 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerStateManager : MonoBehaviour
+namespace State_Machine
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
+    public class PlayerStateManager : MonoBehaviour
     {
         
     }
